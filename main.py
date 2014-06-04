8d37f15cfb11a0de0b76a8aafbf32b48db9e8267
Zhou Xinzi <imdreamrunner@gmail.com>
2014-06-04 18:05:49 +0800

    add query

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import jinja2
 
 
 JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/templates"),
+    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), "templates")),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
